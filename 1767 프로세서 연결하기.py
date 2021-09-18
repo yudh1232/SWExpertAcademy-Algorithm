3f437255e18e6680eb9320550d3eeb92c253acb5
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-09-19 02:38:33 +0900

    Update 1767 프로세서 연결하기.py

--- 1767 프로세서 연결하기.py
+++ 1767 프로세서 연결하기.py
@@ -110,3 +110,107 @@ for tc in range(1, TC + 1):
     # 결과 출력
     answer = min_wire_length
     print('#', tc, ' ', answer, sep = '')
+
+    
+# 개선된 알고리즘 (deepcopy 사용 x)
+"""
+# 상, 우, 하, 좌
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 전선 설치
+def set_wire(core_number, connected_core, wire_length, wire):
+    global max_connected
+    global min_wire_length
+
+    # 가지치기(남은 코어를 모두 연결해도 최대로 연결된 코어 수보다 작을 때)
+    if core_count - core_number + connected_core < max_connected:
+        return
+
+    # 모든 코어를 다 체크했을 때
+    if core_number == core_count:
+        if connected_core > max_connected:
+            max_connected = connected_core
+            min_wire_length = wire_length
+        elif connected_core == max_connected:
+            min_wire_length = min(min_wire_length, wire_length)
+        return
+
+    x = cores[core_number][0]
+    y = cores[core_number][1]
+
+    # 가장자리에 있는 코어는 연결
+    if x == 0 or x == n - 1 or y == 0 or y == n - 1:
+        # 다음 코어 체크하러 감
+        set_wire(core_number + 1, connected_core + 1, wire_length, wire)
+    # 가장자리에 있지 않은 코어
+    else:
+        # 전선을 연결하지 않고 다음 코어 체크
+        set_wire(core_number + 1, connected_core, wire_length, wire)
+        # 상우하좌에 대해
+        for d in range(4):
+            count = 0
+            nx = cores[core_number][0]
+            ny = cores[core_number][1]
+            # 전선 설치
+            while True:
+                nx += dx[d]
+                ny += dy[d]
+                if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                    set_wire(core_number + 1, connected_core + 1, wire_length + count, wire)
+                    break
+                elif board[nx][ny] == 1 or wire[nx][ny] == 1:
+                    break
+                wire[nx][ny] = 1
+                count += 1
+            
+            # 전선 해체
+            d = (d + 2) % 4
+            while count > 0:
+                nx += dx[d]
+                ny += dy[d]
+                wire[nx][ny] = 0
+                count -= 1
+
+# 테스트 케이스 수를 입력받음
+TC = int(input())
+
+# 테스트 케이스 수 만큼 반복
+for tc in range(1, TC + 1):
+    # n을 입력받음
+    n = int(input())
+    
+    # 보드판을 입력받음
+    board = []
+    for _ in range(n):
+        board.append(list(map(int, input().split())))
+    
+    # 전선을 나타내는 리스트 생성
+    wire = [[0] * n for _ in range(n)]
+
+    # 코어의 좌표를 저장하는 리스트
+    cores = []
+
+    # 코어의 개수를 나타내는 변수
+    core_count = 0
+
+    # 코어의 정보를 저장함
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                cores.append((i, j))
+                core_count += 1
+    
+    # 최대로 연결된 코어 수
+    max_connected = 0
+
+    # 코어를 최대로 연결했을 때 최소 전선 길이
+    min_wire_length = int(1e9)
+
+    # 전선 설치(재귀)
+    set_wire(0, 0, 0, wire)
+
+    # 결과 출력
+    answer = min_wire_length
+    print('#', tc, ' ', answer, sep = '')
+"""
